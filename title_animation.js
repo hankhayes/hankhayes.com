9ecc1acd9d621d33ab79983eceeae50000f5a86b
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-06-18 21:38:53 -0500

    Added header image and animation script

---
+++ title_animation.js
@@ -0,0 +1,9 @@
+const textElement = document.getElementById("animatedText");
+
+// Function to start the text animation
+function startTextAnimation() {
+textElement.style.animation = "textAnimation 3s ease-in-out";
+}
+
+// Call the animation function after a delay
+startTextAnimation;
