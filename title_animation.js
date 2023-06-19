8ddd14e0947d6d4f9efadaacac2953a881ae011a
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-06-18 22:27:31 -0500

    Updated some css formatting and changed animation

--- title_animation.js
+++
@@ -1,9 +0,0 @@
-const textElement = document.getElementById("animatedText");
-
-// Function to start the text animation
-function startTextAnimation() {
-textElement.style.animation = "textAnimation 3s ease-in-out";
-}
-
-// Call the animation function after a delay
-startTextAnimation;
