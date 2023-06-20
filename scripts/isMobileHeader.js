1f49988ce0fec814b6a865bc84be6aab31411084
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-06-20 12:35:44 -0500

    Fixed scripts

--- scripts/isMobileHeader.js
+++ scripts/isMobileHeader.js
@@ -1,10 +1,10 @@
-function isMobileTC() {
+function isMobileHeader() {
     return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 }
 
 const headerElement = document.getElementById('mainTC');
 
-if (isMobileTC()) {
+if (isMobileHeader()) {
     headerElement.classList.add('mobile');
 } else {
     headerElement.classList.add('desktop');
