b755f1235fb60ed7e03a17ae6d7a4fb19fb648d5
hankhayes <105171359+hankhayes@users.noreply.github.com>
2023-07-14 18:28:42 -0600

    Fixed image preloading

---
+++ scripts/preloadImages.js
@@ -0,0 +1,34 @@
+var austin = new Image();
+var projects = new Image();
+var tower = new Image();
+var utaustin = new Image();
+
+// Set the source of the image
+austin.src = 'images/austin.jpeg';
+projects.src = 'images/projects.jpeg';
+tower.src = 'images/tower.jpeg';
+utaustin.src = 'images/utaustin.jpeg'
+
+// Preload the image
+austin.onload = function() {
+    // The image has been preloaded
+    console.log('Image preloaded!');
+};
+
+// Preload the image
+projects.onload = function() {
+    // The image has been preloaded
+    console.log('Image preloaded!');
+};
+
+// Preload the image
+tower.onload = function() {
+    // The image has been preloaded
+    console.log('Image preloaded!');
+};
+
+// Preload the image
+utaustin.onload = function() {
+    // The image has been preloaded
+    console.log('Image preloaded!');
+};
